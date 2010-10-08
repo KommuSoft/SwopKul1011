be9de3a7485ef13e94baa19ce38758513c8674b1
Gonaz <jonasvanthornhout@gmail.com>
2010-10-08 11:53:04 +0000

    Test if branching works.
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@10 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

---
+++ Branches/testBranch/ProjectSWOP20102011/src/projectswop20102011/Main.java
@@ -0,0 +1,15 @@
+package projectswop20102011;
+
+/**
+ * @author Willem Van Onsem, Pieter-Jan Vuylsteke and Jonas Vanthornhout
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
