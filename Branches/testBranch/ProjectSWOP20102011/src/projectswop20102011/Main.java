41691b0501afab588a3565093340a3873aae8e49
Gonaz <jonasvanthornhout@gmail.com>
2010-10-08 11:54:35 +0000

    Hello branch.
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@11 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

--- Branches/testBranch/ProjectSWOP20102011/src/projectswop20102011/Main.java
+++ Branches/testBranch/ProjectSWOP20102011/src/projectswop20102011/Main.java
@@ -9,7 +9,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hello branch");
     }
 
 }
