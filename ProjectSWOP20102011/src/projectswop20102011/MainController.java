f90b9ece44b4e761da80293e2fdffabbf9e053af
KommuSoft <vanonsem.willem@gmail.com>
2010-10-17 15:16:47 +0000

    basis modellering en specificatie van de MainController.java
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@20 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

---
+++ ProjectSWOP20102011/src/projectswop20102011/MainController.java
@@ -0,0 +1,39 @@
+package projectswop20102011;
+
+/**
+ * A class that handles all text based user interface. And redirects these expressions
+ * to the right actor controller. Redirection is done on a reflection based way.
+ * @author Willem Van Onsem, Jonas Vanthornhout and Pieter-Jan Vuylsteke
+ */
+public class MainController {
+
+    private static boolean actorControllersLoaded = false;
+
+    private MainController () {}
+
+    /**
+     * Tests if the actor controllers are already loaded
+     * @return true if the actor controllers are loaded, otherwise false.
+     */
+    public static boolean areActorControllersLoaded () {
+        return actorControllersLoaded;
+    }
+    /**
+     * A static method that browses the program in search for actor controllers, and
+     * add them to the list.
+     * This method should be called before the ReadInput method can be called.
+     */
+    public static void LoadActorControllers () {
+
+    }
+
+    /**
+     * Reads input from the text based user interface.
+     * @param expression the expression that has been inserted in the user interface.
+     * @pre the actor controllers must be loaded. | areActorControllersLoaded()
+     */
+    public static void ReadInput (String expression) {
+        
+    }
+
+}
