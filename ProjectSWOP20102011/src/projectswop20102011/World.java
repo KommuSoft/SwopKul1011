85e70fcfdbbe4e6ecfa2fa080666699e3f6ebccb
pjvuylsteke <pjvuylsteke@hotmai.com>
2010-10-23 15:06:39 +0000

    Een setter geschreven voor het attribuut emergencyList van de klasse world. Het staat niet final, dus kmocht da doen? Kheb ze ook gedocumenteerd. Kheb ook een klassendiagram gemaakt. Alleen ben ik nog altijd niet zo goed mee rond het "gebied" Maincontroller, OperatorController, World. Kheb het op het gevoel gedaan. Zou er dan iemand kunnen bevestigen wa er juist of fout is (als ge tijd hebt natuurlijk). Kan dan ook aan de sequence diagrams beginnen. Als het goed is, zal ik nog een beetje documenteren waar nodig in de verschillende klassen.
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@44 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

--- ProjectSWOP20102011/src/projectswop20102011/World.java
+++ ProjectSWOP20102011/src/projectswop20102011/World.java
@@ -1,16 +1,38 @@
 package projectswop20102011;
 /**
+ * A class that represents a world
  *
  * @author Willem Van Onsem, Jonas Vanthornhout & Pieter-Jan Vuylsteke.
  */
 public class World {
 
+    /**
+     * A variable that registers the emergencylist of this world.
+     */
     private EmergencyList emergencyList;
 
+	/**
+	 * Creates a new world
+	 * 
+	 */
     public World () {
-        this.emergencyList = new EmergencyList();
+        setEmergencyList(new EmergencyList());
     }
 
+	/**
+     * Sets the emergencylist of this world.
+     * @param emergencyList
+     *		The emergencylist of this world.
+	 */
+	private void setEmergencyList(EmergencyList emergencyList){
+		this.emergencyList = emergencyList;
+	}
+
+	/**
+     * Returns the emergencylist of this world.
+	 * 
+     * @return The emergencylist of this world.
+	 */
     public EmergencyList getEmergencyList () {
         return this.emergencyList;
     }
