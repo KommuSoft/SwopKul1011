3cf59dfbd634b0ed9ec4633381117d97c471b895
KommuSoft <vanonsem.willem@gmail.com>
2010-11-09 01:20:26 +0000

    +Controllers
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@160 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

--- ProjectSWOP20102011/src/projectswop20102011/userinterface/DispatchUnitsUserInterface.java
+++ ProjectSWOP20102011/src/projectswop20102011/userinterface/DispatchUnitsUserInterface.java
@@ -5,10 +5,12 @@ import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import projectswop20102011.Emergency;
+import projectswop20102011.Unit;
 import projectswop20102011.UnitsNeeded;
 import projectswop20102011.controllers.DispatchUnitsController;
 import projectswop20102011.exceptions.InvalidCommandNameException;
 import projectswop20102011.exceptions.InvalidControllerException;
+import projectswop20102011.exceptions.InvalidEmergencyException;
 import projectswop20102011.exceptions.ParsingAbortedException;
 import projectswop20102011.userinterface.parsers.LongParser;
 
@@ -41,17 +43,30 @@ public class DispatchUnitsUserInterface extends CommandUserInterface {
             if (selectedEmergency == null) {
                 this.writeOutput("Emergency not found.");
             } else {
-                Hashtable<String,String> information = selectedEmergency.toLongInformationString();
+                Hashtable<String, String> information = selectedEmergency.toLongInformationString();
                 this.writeOutput("EMERGENCY DETAILS:");
-                for(Entry<String,String> entry : information.entrySet()) {
-                    this.writeOutput(String.format("\t%s: %s", entry.getKey(),entry.getValue()));
+                this.writeOutput(String.format("\ttype: %s", selectedEmergency.getClass().getSimpleName()));
+                for (Entry<String, String> entry : information.entrySet()) {
+                    this.writeOutput(String.format("\t%s: %s", entry.getKey(), entry.getValue()));
                 }
                 this.writeOutput("NEEDED UNITS:");
                 UnitsNeeded unitsNeeded = this.controller.getNeededUnits(selectedEmergency);
                 Class[] types = unitsNeeded.getUnits();
                 long[] needed = unitsNeeded.getNumbersNeeded();
-                for(int i = 0; i < types.length; i++) {
-                    this.writeOutput(String.format("\t%s: %s", types[i].getSimpleName(),needed[i]));
+                for (int i = 0; i < types.length; i++) {
+                    this.writeOutput(String.format("\t%s: %s", types[i].getSimpleName(), needed[i]));
+                }
+                this.writeOutput("AVAILABLE UNITS:");
+                Unit[] availableUnits;
+                try {
+                    availableUnits = this.controller.getAvailableUnitsSorted(selectedEmergency);
+                    for (int i = 0; i < availableUnits.length; i++) {
+                        Unit u = availableUnits[i];
+                        double distance = u.getCurrentLocation().getDistanceTo(selectedEmergency.getLocation());
+                        this.writeOutput(String.format("\t%s\t%s\t%s\t%s", i, u.getClass().getSimpleName(), u.getName(), distance));
+                    }
+                } catch (InvalidEmergencyException ex) {
+                    this.writeOutput(String.format("ERROR: %s", ex.getMessage()));
                 }
             }
         } catch (ParsingAbortedException ex) {
