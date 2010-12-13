438ada20d25d0800e0680f0eca4545514e470bf5
KommuSoft <vanonsem.willem@gmail.com>
2010-12-13 12:37:57 +0000

    implementatie Policy (part II)
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@241 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

--- ProjectSWOP20102011/src/projectswop20102011/utils/parsers/EnumParser.java
+++ ProjectSWOP20102011/src/projectswop20102011/utils/parsers/EnumParser.java
@@ -1,18 +1,58 @@
 package projectswop20102011.utils.parsers;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import projectswop20102011.exceptions.ParsingException;
 import projectswop20102011.utils.Parser;
 
 /**
  * A parser that can parse enumeration values with a given enumeration.
  * @author Willem Van Onsem, Jonas Vanthornhout & Pieter-Jan Vuylsteke
  */
-public abstract class EnumParser<T> extends Parser<T> {
+public class EnumParser<T extends Enum> extends Parser<T> {
 
-    public EnumParser (Class<? extends Enum>) {
-        super(makeRegex(enumeration));
+    public EnumParser(Class<T> enumClass) {
+        super(makeRegex(enumClass));
     }
 
-    private static String makeRegex (Enum enumeration) {
+    private static <T extends Enum> String makeRegex(Class<T> enumClass) {
+        try {
+            Method m = enumClass.getMethod("values", new Class[0]);
+            T[] elements = (T[]) m.invoke(null, new Object[0]);
+            if (elements.length > 0) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(elements[0].name());
+                for(int i = 1; i < elements.length; i++) {
+                    sb.append(String.format("|%s",elements[i].name()));
+                }
+                System.out.println(sb.toString());
+                return sb.toString();
+            }
+        } catch (NoSuchMethodException ex) {
+            //We assume this can't happen
+            Logger.getLogger(EnumParser.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            //We assume this can't happen
+            Logger.getLogger(EnumParser.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            //We assume this can't happen
+            Logger.getLogger(EnumParser.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            //We assume this can't happen
+            Logger.getLogger(EnumParser.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvocationTargetException ex) {
+            //We assume this can't happen
+            Logger.getLogger(EnumParser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "";
     }
 
+    @Override
+    protected T parseFromMatcher(Matcher matcher) throws ParsingException {
+        return null;
+    }
 }
---
+++ ProjectSWOP20102011/test/projectswop20102011/utils/parsers/EnumParserTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projectswop20102011.utils.parsers;
+
+import projectswop20102011.domain.FireSize;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author willem
+ */
+public class EnumParserTest {
+
+    public EnumParserTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testConstructor() {
+        new EnumParser(FireSize.class);
+    }
+    
+
+}
