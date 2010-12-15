e3903e81c3729d2ed2893918ec77094a4cb4822b
KommuSoft <vanonsem.willem@gmail.com>
2010-12-15 22:17:43 +0000

    implementatie van de policies
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@273 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

--- ProjectSWOP20102011/src/projectswop20102011/domain/ASAPDispatchPolicy.java
+++ ProjectSWOP20102011/src/projectswop20102011/domain/ASAPDispatchPolicy.java
@@ -15,7 +15,7 @@ public class ASAPDispatchPolicy extends DispatchPolicy {
      * @throws InvalidUnitsNeededException If the given UnitsNeeded object is not effective.
      * @throws InvalidDispatchPolicyException If the given UnitsNeeded object has already a Policy.
      */
-    public ASAPDispatchPolicy (UnitsNeeded unitsNeeded) throws InvalidUnitsNeededException, InvalidDispatchPolicyException {
+    public ASAPDispatchPolicy(UnitsNeeded unitsNeeded) throws InvalidUnitsNeededException, InvalidDispatchPolicyException {
         super(unitsNeeded);
     }
 
@@ -34,12 +34,13 @@ public class ASAPDispatchPolicy extends DispatchPolicy {
      * Compares two different units by there expected time of arrival (ETA)
      * @param unit1 The first unit to compare.
      * @param unit2 The second unit to compare.
-     * @return a negative integer, zero, or a positive integer as the first unit is more, equal or less intresting than the unit according to this Policy.
+     * @return a negative integer, zero, or a positive integer as the first unit is more,
+     *          equal or less intresting than the unit according to this Policy. This means
+     *          that the unit with the lowest Expected Time of Arrival will be more intresting.
      */
     @Override
-    public int compare (Unit unit1, Unit unit2) {
+    public int compare(Unit unit1, Unit unit2) {
         GPSCoordinate emergencyLocation = this.getUnitsNeeded().getEmergency().getLocation();
         return ((Long) unit1.getETA(emergencyLocation)).compareTo(unit2.getETA(emergencyLocation));
     }
-
 }
--- ProjectSWOP20102011/src/projectswop20102011/domain/DefaultDispatchPolicy.java
+++ ProjectSWOP20102011/src/projectswop20102011/domain/DefaultDispatchPolicy.java
@@ -31,9 +31,18 @@ public class DefaultDispatchPolicy extends DispatchPolicy {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * Compares two different units by the distance to the location of the emergency.
+     * @param unit1 The first unit to compare.
+     * @param unit2 The second unit to compare.
+     * @return a negative integer, zero, or a positive integer as the first unit is more,
+     *          equal or less intresting than the unit according to this Policy. This means
+     *          that the unit with the lowest distqnce to the emergency will be more intresting.
+     */
     @Override
-    public int compare(Unit o1, Unit o2) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public int compare(Unit unit1, Unit unit2) {
+        GPSCoordinate emergencyLocation = this.getUnitsNeeded().getEmergency().getLocation();
+        return ((Double) unit1.getDistanceTo(emergencyLocation)).compareTo(unit2.getDistanceTo(emergencyLocation));
     }
 
 }
