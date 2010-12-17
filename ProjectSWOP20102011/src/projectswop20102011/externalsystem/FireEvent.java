b6800397eff87cc9818cae15265e25e6cd569059
pjvuylsteke <pjvuylsteke@hotmai.com>
2010-12-17 01:03:05 +0000

    Throwtje toegevoegd
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@295 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

--- ProjectSWOP20102011/src/projectswop20102011/externalsystem/FireEvent.java
+++ ProjectSWOP20102011/src/projectswop20102011/externalsystem/FireEvent.java
@@ -6,6 +6,7 @@ import projectswop20102011.domain.EmergencySeverity;
 import projectswop20102011.domain.FireSize;
 import projectswop20102011.domain.GPSCoordinate;
 import projectswop20102011.domain.World;
+import projectswop20102011.exceptions.InvalidAmountOfParametersException;
 import projectswop20102011.exceptions.InvalidEmergencySeverityException;
 import projectswop20102011.exceptions.InvalidEmergencyTypeNameException;
 import projectswop20102011.exceptions.InvalidFireSizeException;
@@ -16,7 +17,7 @@ import projectswop20102011.factories.FireFactory;
 
 public class FireEvent extends Event {
 
-	public FireEvent(ITime time, World world, Location location, EmergencySeverity severity, String description, FireSize size, boolean chemical, long trappedPeople, long numberOfInjured) throws InvalidWorldException, InvalidLocationException, InvalidEmergencySeverityException, InvalidFireSizeException, NumberOutOfBoundsException, InvalidEmergencyTypeNameException, InvalidParameterException {
+	public FireEvent(ITime time, World world, Location location, EmergencySeverity severity, String description, FireSize size, boolean chemical, long trappedPeople, long numberOfInjured) throws InvalidWorldException, InvalidLocationException, InvalidEmergencySeverityException, InvalidFireSizeException, NumberOutOfBoundsException, InvalidEmergencyTypeNameException, InvalidParameterException, InvalidAmountOfParametersException {
 		super(time, location, null, world);
 
 		FireFactory ff = new FireFactory();
