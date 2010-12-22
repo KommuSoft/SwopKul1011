a7b47048dcc8ab1f2f1223508bfff689faa651ea
pjvuylsteke <pjvuylsteke@hotmai.com>
2010-12-22 23:43:21 +0000

    Lijntje this.writeOutput... in de try gezet
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@431 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

--- ProjectSWOP20102011/src/projectswop20102011/userinterface/RemoveUnitAssignmentInterface.java
+++ ProjectSWOP20102011/src/projectswop20102011/userinterface/RemoveUnitAssignmentInterface.java
@@ -17,68 +17,69 @@ import projectswop20102011.userinterface.parsers.StringParser;
  */
 public class RemoveUnitAssignmentInterface extends CommandUserInterface {
 
-    private final RemoveUnitAssignmentController controller;
+	private final RemoveUnitAssignmentController controller;
 
-    /**
-     *
-     * @param controller
-     * @throws InvalidCommandNameException
-     * @throws InvalidControllerException
-     */
-    public RemoveUnitAssignmentInterface(RemoveUnitAssignmentController controller) throws InvalidCommandNameException, InvalidControllerException {
-        super("remove unit assignment");
-        if (controller == null) {
-            throw new InvalidControllerException("Controller must be effective.");
-        }
-        this.controller = controller;
-    }
+	/**
+	 *
+	 * @param controller
+	 * @throws InvalidCommandNameException
+	 * @throws InvalidControllerException
+	 */
+	public RemoveUnitAssignmentInterface(RemoveUnitAssignmentController controller) throws InvalidCommandNameException, InvalidControllerException {
+		super("remove unit assignment");
+		if (controller == null) {
+			throw new InvalidControllerException("Controller must be effective.");
+		}
+		this.controller = controller;
+	}
 
-    @Override
-    public RemoveUnitAssignmentController getController() {
-        return this.controller;
-    }
+	@Override
+	public RemoveUnitAssignmentController getController() {
+		return this.controller;
+	}
 
-    @Override
-    public void HandleUserInterface() {
-        long emergencyId = 0;
-        try {
-            emergencyId = this.parseInputToType(new LongParser(), "The id of the emergency");
-        } catch (ParsingAbortedException ex) {
-            writeOutput(String.format("ERROR: %s", ex.getMessage()));
-        }
-        Emergency selectedEmergency = this.getController().getEmergencyFromId(emergencyId);
-        if (selectedEmergency == null) {
-            this.writeOutput("Emergency not found.");
-        } else {
-            this.writeOutput("WORKING UNITS:");
-            //TODO: Komt niet goed overeen met de lijst van units die eronder staan:  this.writeOutput(String.format("\t%s\t%s\t%s", "id", "Unit type", "Unit name"));
-            ArrayList<Unit> workingUnits = this.getController().getWorkingUnits(selectedEmergency);
-            if (workingUnits.isEmpty()) {
-                this.writeOutput("ERROR: There are no working units.");
-            } else {
-                for (int i = 0; i < workingUnits.size(); i++) {
-                    this.writeOutput(String.format("\t%s\t%s\t%s", i, workingUnits.get(i).getClass().getSimpleName(), workingUnits.get(i).getName()));
-                }
-                String expression = null;
-                do {
-                    this.writeOutput("Type in a unit id, or type \"stop\" to finish the list.");
-                    try {
-                        expression = this.parseInputToType(new StringParser(), "id");
-                    } catch (ParsingAbortedException ex) {
-                        writeOutput(String.format("ERROR: %s", ex.getMessage()));
-                    }
-                    if (!expression.equals("stop")) {
-                        int id = Integer.parseInt(expression);
-                        try {
-                            this.getController().withdrawUnit(workingUnits.get(id));
-                        } catch (Exception e) {
-                            writeOutput(String.format("ERROR: %s", e.getMessage()));
-                        }
-                        this.writeOutput("Unit removed.");
-                    }
-                } while (!expression.equals("stop"));
-                this.writeOutput("Units are removed");
-            }
-        }
-    }
+	@Override
+	public void HandleUserInterface() {
+		long emergencyId = 0;
+		try {
+			emergencyId = this.parseInputToType(new LongParser(), "The id of the emergency");
+		} catch (ParsingAbortedException ex) {
+			writeOutput(String.format("ERROR: %s", ex.getMessage()));
+		}
+		Emergency selectedEmergency = this.getController().getEmergencyFromId(emergencyId);
+		if (selectedEmergency == null) {
+			this.writeOutput("Emergency not found.");
+		} else {
+			this.writeOutput("WORKING UNITS:");
+			//TODO: Komt niet goed overeen met de lijst van units die eronder staan:  this.writeOutput(String.format("\t%s\t%s\t%s", "id", "Unit type", "Unit name"));
+			ArrayList<Unit> workingUnits = this.getController().getWorkingUnits(selectedEmergency);
+			if (workingUnits.isEmpty()) {
+				this.writeOutput("ERROR: There are no working units.");
+			} else {
+				for (int i = 0; i < workingUnits.size(); i++) {
+					this.writeOutput(String.format("\t%s\t%s\t%s", i, workingUnits.get(i).getClass().getSimpleName(), workingUnits.get(i).getName()));
+				}
+				String expression = null;
+				do {
+					this.writeOutput("Type in a unit id, or type \"stop\" to finish the list.");
+					try {
+						expression = this.parseInputToType(new StringParser(), "id");
+					} catch (ParsingAbortedException ex) {
+						writeOutput(String.format("ERROR: %s", ex.getMessage()));
+					}
+					if (!expression.equals("stop")) {
+						int id = Integer.parseInt(expression);
+						try {
+							this.getController().withdrawUnit(workingUnits.get(id));
+							this.writeOutput("Unit removed.");
+						} catch (Exception e) {
+							writeOutput(String.format("ERROR: %s", e.getMessage()));
+						}
+
+					}
+				} while (!expression.equals("stop"));
+				this.writeOutput("Units are removed");
+			}
+		}
+	}
 }
