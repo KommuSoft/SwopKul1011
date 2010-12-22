f92a2efb5d045459af5ddc35bc884d72a3252065
KommuSoft <vanonsem.willem@gmail.com>
2010-12-22 09:33:22 +0000

    testen werken weer
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@374 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

--- ProjectSWOP20102011/test/projectswop20102011/factories/FiretruckFactoryTest.java
+++ ProjectSWOP20102011/test/projectswop20102011/factories/FiretruckFactoryTest.java
@@ -3,9 +3,11 @@ package projectswop20102011.factories;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import projectswop20102011.domain.FireSize;
 import projectswop20102011.domain.Firetruck;
 import projectswop20102011.domain.GPSCoordinate;
 import projectswop20102011.exceptions.InvalidAmountOfParametersException;
+import projectswop20102011.exceptions.InvalidFireSizeException;
 import projectswop20102011.exceptions.InvalidLocationException;
 import projectswop20102011.exceptions.InvalidMapItemNameException;
 import projectswop20102011.exceptions.InvalidMapItemTypeNameException;
@@ -17,6 +19,7 @@ public class FiretruckFactoryTest {
 	private String name1;
 	private GPSCoordinate homeLocation1;
 	private long speed1;
+        private FireSize fireSize1;
 	private FiretruckFactory ftf;
 
 	@Before
@@ -26,6 +29,7 @@ public class FiretruckFactoryTest {
 		name1 = "Brandweerwagen";
 		homeLocation1 = new GPSCoordinate(x1,y1);
 		speed1 = 5;
+                fireSize1 = FireSize.FACILITY;
 	}
 
 	@Test
@@ -34,19 +38,19 @@ public class FiretruckFactoryTest {
 	}
 
 	@Test
-	public void testCreateMapItem() throws InvalidMapItemTypeNameException, InvalidLocationException, InvalidSpeedException, InvalidMapItemNameException, InvalidAmountOfParametersException {
+	public void testCreateMapItem() throws InvalidMapItemTypeNameException, InvalidLocationException, InvalidSpeedException, InvalidMapItemNameException, InvalidAmountOfParametersException, InvalidFireSizeException {
 		ftf = new FiretruckFactory();
 
-		Firetruck ft = ftf.createMapItem(new Object[] {name1, homeLocation1, speed1});
+		Firetruck ft = ftf.createMapItem(new Object[] {name1, homeLocation1, speed1,fireSize1});
 		assertEquals(name1, ft.getName());
 		assertEquals(homeLocation1, ft.getHomeLocation());
 		assertEquals(speed1, ft.getSpeed());
+                assertEquals(fireSize1,ft.getMaxSize());
 	}
 
 	@Test(expected = InvalidAmountOfParametersException.class)
-	public void testCreateMapItemInvalidAmount() throws InvalidMapItemTypeNameException, InvalidLocationException, InvalidSpeedException, InvalidMapItemNameException, InvalidAmountOfParametersException {
+	public void testCreateMapItemInvalidAmount() throws InvalidMapItemTypeNameException, InvalidLocationException, InvalidSpeedException, InvalidMapItemNameException, InvalidAmountOfParametersException, InvalidFireSizeException {
 		ftf = new FiretruckFactory();
-
 		Firetruck ft = ftf.createMapItem(new Object[] {name1, homeLocation1});
 	}
 }
