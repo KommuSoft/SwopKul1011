7d402cb268a4705b27235d8c8aeb2adb7c29e423
KommuSoft <vanonsem.willem@gmail.com>
2010-12-22 12:27:03 +0000

    Extra sugar voor de constraints
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@390 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

--- ProjectSWOP20102011/src/projectswop20102011/domain/validators/DispatchUnitsConstraint.java
+++ ProjectSWOP20102011/src/projectswop20102011/domain/validators/DispatchUnitsConstraint.java
@@ -16,6 +16,7 @@ public abstract class DispatchUnitsConstraint {
     /**
      * Checks if the given list of units passes this constraint and all units are relevant to this constraint.
      * @param units The list of units to check.
+     * @pre All the elements in units are effective.
      * @return True if the given list passes the constraints and all the units are used, otherwise false.
      */
     public boolean areAllUnitsReleventAndConstraintPassed(Collection<Unit> units) {
@@ -26,6 +27,7 @@ public abstract class DispatchUnitsConstraint {
     /**
      * Tests if all the given units in the list are relevant for the constraint.
      * @param units The list of units to check.
+     * @pre All the elements in units are effective.
      * @return True if all units are relevant for the constraint, otherwise false.
      */
     public boolean areAllUnitsRelevant(Collection<Unit> units) {
@@ -34,6 +36,12 @@ public abstract class DispatchUnitsConstraint {
         return relevant.containsAll(units);
     }
 
+    /**
+     * Checks if this constraint passes by the given units (not all the units need te be relevant however).
+     * @param units The list of units to check.
+     * @pre All the units are effective.
+     * @return True if this constraint passes by the given units.
+     */
     public boolean areValidDispatchUnits(Collection<Unit> units) {
         return areValidDispatchUnits(new ArrayList<Unit>(units), new HashSet<Unit>());
     }
@@ -42,7 +50,7 @@ public abstract class DispatchUnitsConstraint {
      * Tests if the given Iterable object of units could be allocated to the emergency where this DispatchUnitsConstraint is part of.
      * @param units An iterable object containing only unique and only effective units.
      * @param relevantUnits A list where Units out of units are added to if they are relevant to met the contstraint.
-     * @pre The given units parameter contains only unique (no duplicates) effective units.
+     * @pre The given units parameter contains only effective units (duplicates are allowed).
      * @return True if this constraint passes with the given iterable of units, otherwise false.
      * @note The implementation of this method needs to be designed that the units in front of the units list will be added first to the relevantUnits.
      */
@@ -52,6 +60,7 @@ public abstract class DispatchUnitsConstraint {
      * Generates a proposal where all the elements of the fixed part are used, and where some of the variableParts are used. The proposal tries to pass the constraint, if that's impossible all Units that
      * @param fixedPart A list of units, where all the elements need to be used.
      * @param variablePart A list of units where some (or even all/none) units are selected.
+     * @pre Both the fixed and the variable part don't contain ineffective elements.
      * @return If the fixed part is completly used a subset of the varialbePart containing a selection of units that are relevant fo the constraint. Although a combination of this selection and the fixed part doesn't need to pass the constraint. Otherwise an empty list.
      */
     public Collection<Unit> generateProposal(List<Unit> fixedPart, List<Unit> variablePart) {
