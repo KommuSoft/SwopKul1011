2b2245b3fcb0003afc18485421541ec7c3ee34f4
KommuSoft <vanonsem.willem@gmail.com>
2011-03-10 21:55:04 +0000

    scenario1 vervolg part I
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@561 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

--- ProjectSWOP20102011/test/projectswop20102011/scenarios/Scenario1Test.java
+++ ProjectSWOP20102011/test/projectswop20102011/scenarios/Scenario1Test.java
@@ -22,10 +22,13 @@ import projectswop20102011.domain.FireSize;
 import projectswop20102011.domain.Firetruck;
 import projectswop20102011.domain.GPSCoordinate;
 import projectswop20102011.domain.Unit;
+import projectswop20102011.domain.lists.EmergencyFactoryList;
+import projectswop20102011.domain.lists.ParserList;
 import projectswop20102011.exceptions.InvalidAmountOfParametersException;
 import projectswop20102011.exceptions.InvalidEmergencyException;
 import projectswop20102011.exceptions.InvalidEmergencySeverityException;
 import projectswop20102011.exceptions.InvalidEmergencyStatusException;
+import projectswop20102011.exceptions.InvalidEmergencyTypeNameException;
 import projectswop20102011.exceptions.InvalidFireSizeException;
 import projectswop20102011.exceptions.InvalidLocationException;
 import projectswop20102011.exceptions.InvalidMapItemNameException;
@@ -34,6 +37,17 @@ import projectswop20102011.exceptions.InvalidSpeedException;
 import projectswop20102011.exceptions.InvalidWorldException;
 import projectswop20102011.exceptions.NumberOutOfBoundsException;
 import projectswop20102011.externalsystem.EmergencyDispatchApi;
+import projectswop20102011.factories.FireFactory;
+import projectswop20102011.factories.PublicDisturbanceFactory;
+import projectswop20102011.factories.RobberyFactory;
+import projectswop20102011.factories.TrafficAccidentFactory;
+import projectswop20102011.utils.parsers.BooleanParser;
+import projectswop20102011.utils.parsers.EmergencySeverityParser;
+import projectswop20102011.utils.parsers.FireSizeParser;
+import projectswop20102011.utils.parsers.GPSCoordinateParser;
+import projectswop20102011.utils.parsers.IntegerParser;
+import projectswop20102011.utils.parsers.LongParser;
+import projectswop20102011.utils.parsers.StringParser;
 
 /**
  *
@@ -49,59 +63,81 @@ public class Scenario1Test {
     private TimeAheadController tac;
 
     @Before
-    public void setUp() throws InvalidWorldException {
+    public void setUp() throws InvalidWorldException, InvalidEmergencyTypeNameException {
         World world = new World();
+        EmergencyFactoryList efl = world.getEmergencyFactoryList();
+        efl.addEmergencyFactory(new FireFactory());
+        efl.addEmergencyFactory(new TrafficAccidentFactory());
+        efl.addEmergencyFactory(new RobberyFactory());
+        efl.addEmergencyFactory(new PublicDisturbanceFactory());
+        ParserList pl = world.getParserList();
+        pl.addParser(new BooleanParser());
+        pl.addParser(new EmergencySeverityParser());
+        pl.addParser(new FireSizeParser());
+        pl.addParser(new GPSCoordinateParser());
+        pl.addParser(new IntegerParser());
+        pl.addParser(new LongParser());
+        pl.addParser(new StringParser());
         IEmergencyDispatchApi api = new EmergencyDispatchApi(world);
-	world.setIEmergencyDispatchApi(api);
+        world.setIEmergencyDispatchApi(api);
         cec = new CreateEmergencyController(world);
         iec = new InspectEmergenciesController(world);
         redc = new ReadEnvironmentDataController(world);
         duc = new DispatchUnitsController(world);
         eotc = new EndOfTaskController(world);
-        tac = new TimeAheadController(world,ExternalSystem.bootstrap(api));
+        tac = new TimeAheadController(world, ExternalSystem.bootstrap(api));
         System.out.println();
     }
 
-    
     @Test
-    public void testScenario1 () throws InvalidLocationException, InvalidEmergencySeverityException, InvalidFireSizeException, NumberOutOfBoundsException, InvalidMapItemTypeNameException, InvalidMapItemNameException, InvalidSpeedException, InvalidAmountOfParametersException, InvalidEmergencyException, InvalidEmergencyStatusException, Exception {
+    public void testScenario1() throws InvalidLocationException, InvalidEmergencySeverityException, InvalidFireSizeException, NumberOutOfBoundsException, InvalidMapItemTypeNameException, InvalidMapItemNameException, InvalidSpeedException, InvalidAmountOfParametersException, InvalidEmergencyException, InvalidEmergencyStatusException, Exception {
         //initialize world
-        redc.addFiretruck("engine1", new GPSCoordinate(0,20), 5, FireSize.HOUSE);
+        redc.addFiretruck("engine1", new GPSCoordinate(0, 20), 5, FireSize.HOUSE);
+        redc.addFiretruck("engine2", new GPSCoordinate(0, 100), 5, FireSize.HOUSE);
+        redc.addPolicecar("unit1", new GPSCoordinate(0, 200), 5);
         //inspecting emergencies
-        assertEquals(0,iec.inspectEmergenciesOnStatus(EmergencyStatus.RECORDED_BUT_UNHANDLED).length);
-        assertEquals(0,iec.inspectEmergenciesOnStatus(EmergencyStatus.RESPONSE_IN_PROGRESS).length);
-        assertEquals(0,iec.inspectEmergenciesOnStatus(EmergencyStatus.COMPLETED).length);
+        assertEquals(0, iec.inspectEmergenciesOnStatus(EmergencyStatus.RECORDED_BUT_UNHANDLED).length);
+        assertEquals(0, iec.inspectEmergenciesOnStatus(EmergencyStatus.RESPONSE_IN_PROGRESS).length);
+        assertEquals(0, iec.inspectEmergenciesOnStatus(EmergencyStatus.COMPLETED).length);
         //adding fire
-        cec.createFireEmergency(new GPSCoordinate(0,0), EmergencySeverity.BENIGN,"geïmplodeerde tv", FireSize.HOUSE, false, 0, 2);
+        cec.createFireEmergency(new GPSCoordinate(0, 0), EmergencySeverity.BENIGN, "geïmplodeerde tv", FireSize.LOCAL, false, 0, 0);
         //inspecting emergencies
         Emergency[] rbu_em = iec.inspectEmergenciesOnStatus(EmergencyStatus.RECORDED_BUT_UNHANDLED);
-        assertEquals(1,rbu_em.length);
-        assertEquals(0,iec.inspectEmergenciesOnStatus(EmergencyStatus.RESPONSE_IN_PROGRESS).length);
-        assertEquals(0,iec.inspectEmergenciesOnStatus(EmergencyStatus.COMPLETED).length);
+        assertEquals(1, rbu_em.length);
+        assertEquals(0, iec.inspectEmergenciesOnStatus(EmergencyStatus.RESPONSE_IN_PROGRESS).length);
+        assertEquals(0, iec.inspectEmergenciesOnStatus(EmergencyStatus.COMPLETED).length);
         //assign engine1 to the fire
         List<Unit> units = duc.getAvailableUnitsSorted(rbu_em[0]);
         Firetruck engine1 = (Firetruck) units.get(0);
-        assertEquals("engine1",engine1.getName());
+        assertEquals("engine1", engine1.getName());
         Set<Unit> assign_units = new HashSet<Unit>();
         assign_units.add(engine1);
         duc.dispatchToEmergency(rbu_em[0], assign_units);
         //inspecting emergencies
-        assertEquals(0,iec.inspectEmergenciesOnStatus(EmergencyStatus.RECORDED_BUT_UNHANDLED).length);
-        assertEquals(1,iec.inspectEmergenciesOnStatus(EmergencyStatus.RESPONSE_IN_PROGRESS).length);
-        assertEquals(0,iec.inspectEmergenciesOnStatus(EmergencyStatus.COMPLETED).length);
+        assertEquals(0, iec.inspectEmergenciesOnStatus(EmergencyStatus.RECORDED_BUT_UNHANDLED).length);
+        assertEquals(1, iec.inspectEmergenciesOnStatus(EmergencyStatus.RESPONSE_IN_PROGRESS).length);
+        assertEquals(0, iec.inspectEmergenciesOnStatus(EmergencyStatus.COMPLETED).length);
         try {
             eotc.indicateEndOfTask(engine1);
             fail("engine1 cant end of task");
+        } catch (Exception e) {
         }
-        catch (Exception e) {}
         tac.doTimeAheadAction(18);
         try {
             eotc.indicateEndOfTask(engine1);
             fail("engine1 cant end of task");
+        } catch (Exception e) {
         }
-        catch (Exception e) {}
+        //ADDING EMERGENCIES BY EXTERNAL SYSTEM:
+        //[type=Robbery; assignable=true; status=recorded but unhandled; location=(35,80); working units=[  ]; severity=urgent; type=Robbery; description=]
+        //[type=TrafficAccident; assignable=true; status=recorded but unhandled; location=(-90,5); working units=[  ]; severity=serious; type=TrafficAccident; description=]
+        //[type=PublicDisturbance; assignable=true; status=recorded but unhandled; location=(-10,40); working units=[  ]; severity=normal; type=PublicDisturbance; description=]
+        //[type=Fire; assignable=true; status=recorded but unhandled; location=(90,90); working units=[  ]; severity=normal; type=Fire; description=]
+        //[type=TrafficAccident; assignable=true; status=recorded but unhandled; location=(100,0); working units=[  ]; severity=serious; type=TrafficAccident; description=]
         tac.doTimeAheadAction(14400);
         eotc.indicateEndOfTask(engine1);
+        assertEquals(5, iec.inspectEmergenciesOnStatus(EmergencyStatus.RECORDED_BUT_UNHANDLED).length);
+        assertEquals(0, iec.inspectEmergenciesOnStatus(EmergencyStatus.RESPONSE_IN_PROGRESS).length);
+        assertEquals(1, iec.inspectEmergenciesOnStatus(EmergencyStatus.COMPLETED).length);
     }
-
 }
