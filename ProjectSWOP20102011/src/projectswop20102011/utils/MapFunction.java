7e902a6c268da666e0b8535816146a9bd79f180e
KommuSoft <vanonsem.willem@gmail.com>
2011-03-31 17:14:42 +0000

    Mapper uitgebreid.
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@623 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

--- ProjectSWOP20102011/src/projectswop20102011/utils/MapFunction.java
+++ ProjectSWOP20102011/src/projectswop20102011/utils/MapFunction.java
@@ -5,13 +5,23 @@ import java.util.Collection;
 
 /**
  * A class representing a function that maps objects to other objects.
+ * @param <TSource> The class from where the mapper maps.
+ * @param <TDestination>  The class to where the mapper maps.
  * @author Willem Van Onsem, Jonas Vanthornhout & Pieter-Jan Vuylsteke
  */
 public abstract class MapFunction<TSource,TDestination> {
     
-    public static<TSource,TDestination> ArrayList<TDestination> mapCollection (Collection<TSource> sources, MapFunction<? super TSource,TDestination> function) {
+    /**
+     * Maps a collection of sources to an ArrayList of destinations.
+     * @param <TSource> The type of objects to map from.
+     * @param <TDestination> The type of object to map to.
+     * @param sources A collection of sources to map from.
+     * @param function The function that describes how to map a source to its destination.
+     * @return An ArrayList that contains the products of the mapping of the sources.
+     * @throws Throwable If one of the mappings throws a Throwable.
+     */
+    public static<TSource,TDestination> ArrayList<TDestination> mapCollection (Collection<TSource> sources, MapFunction<? super TSource,TDestination> function) throws Throwable {
         ArrayList<TDestination> result = new ArrayList<TDestination>(sources.size());
-        //TDestination[] result = new TDestination[sources.size()];
         for(TSource x : sources) {
             result.add(function.getFunctionResult(x));
         }
@@ -22,7 +32,8 @@ public abstract class MapFunction<TSource,TDestination> {
      * An abstract method to represent the mapping function.
      * @param source The source to calculate the destination from.
      * @return The destination object from the source.
+     * @throws Throwable If something goes wrong when mapping.
      */
-    public abstract TDestination getFunctionResult (TSource source);
+    public abstract TDestination getFunctionResult (TSource source) throws Throwable;
 
 }
