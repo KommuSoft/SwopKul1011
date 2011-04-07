2dc2a2a014c4ea0876e38b1a9ab0973056b6b859
pjvuylsteke <pjvuylsteke@hotmai.com>
2011-04-07 12:15:16 +0000

    Nieuwe controllers toegevoegd met betrekking tot de nieuwe use cases (create disaster en dispatch units to disaster) en een disastermapper
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@655 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

---
+++ ProjectSWOP20102011/src/projectswop20102011/controllers/CreateDisasterController.java
@@ -0,0 +1,11 @@
+package projectswop20102011.controllers;
+
+import projectswop20102011.World;
+import projectswop20102011.exceptions.InvalidWorldException;
+
+public class CreateDisasterController extends Controller {
+
+	public CreateDisasterController(World world) throws InvalidWorldException {
+		super(world);
+	}
+}
---
+++ ProjectSWOP20102011/src/projectswop20102011/controllers/DisasterMapper.java
@@ -0,0 +1,11 @@
+package projectswop20102011.controllers;
+
+import projectswop20102011.World;
+import projectswop20102011.exceptions.InvalidWorldException;
+
+public class DisasterMapper extends Controller {
+
+	public DisasterMapper(World world) throws InvalidWorldException {
+		super(world);
+	}
+}
