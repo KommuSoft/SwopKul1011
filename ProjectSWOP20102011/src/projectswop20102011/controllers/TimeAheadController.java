f7eb3de074514c7f8c30d950e5075a6376956ce4
Gonaz <jonasvanthornhout@gmail.com>
2011-05-17 17:50:49 +0000

    Een klasse die niet ze actief gebruikt werd is nu weg.
    git-svn-id: file:///home/kommusoft/tmpgit/swopsvn@776 c49496ef-df08-40b3-bdd7-5dbf4c3ba35c

--- ProjectSWOP20102011/src/projectswop20102011/controllers/TimeAheadController.java
+++ ProjectSWOP20102011/src/projectswop20102011/controllers/TimeAheadController.java
@@ -4,18 +4,12 @@ import be.kuleuven.cs.swop.external.IExternalSystem;
 import projectswop20102011.World;
 import projectswop20102011.exceptions.InvalidDurationException;
 import projectswop20102011.exceptions.InvalidWorldException;
-import projectswop20102011.externalsystem.TimeAheadAdapter;
 
 /**
  * A controller used to go forward in time (use case #7).
  * @author Willem Van Onsem, Jonas Vanthornhout & Pieter-Jan Vuylsteke
  */
 public class TimeAheadController extends Controller {
-
-	/**
-	 * A variable registering the TimeAheadAdapter of this controller.
-	 */
-	private final TimeAheadAdapter taa;
 
 	/**
 	 * Creates a new instance of a TimeAhead controller with a given world.
@@ -32,17 +26,7 @@ public class TimeAheadController extends Controller {
 	 */
 	public TimeAheadController(World world, IExternalSystem externalSystem) throws InvalidWorldException {
 		super(world);
-		taa = new TimeAheadAdapter(getWorld().getIEmergencyDispatchApi(), externalSystem);
 	}
-
-	/**
-	 * Gets the TimeAheadAdapter of this TimeAheadController.
-	 * @return The TimaAheadAdapter ot this TimeAheadController.
-	 */
-	private TimeAheadAdapter getTimeAheadAdapter() {
-		return taa;
-	}
-
 	/**
 	 * Performs a time ahead action, where all the time sensitive objects in the world will be modified under a certain time difference.
 	 * @param seconds
@@ -52,7 +36,6 @@ public class TimeAheadController extends Controller {
 	 */
 	public void doTimeAheadAction(long seconds) throws InvalidDurationException {
 		getWorld().getTimeSensitiveList().timeAhead(seconds);
-		getTimeAheadAdapter().timeChange(getWorld().getTime(), seconds);
 		getWorld().setTime(getWorld().getTime() + seconds);
 	}
 }
